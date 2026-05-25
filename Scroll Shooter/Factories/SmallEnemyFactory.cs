11e323e81e65c3fb04ee579e7a6e9a6a0da0eb15
ramalakeven <143877366+ramalakeven@users.noreply.github.com>
2026-05-25 11:04:47 +0300

    Use provided behavior or default to StraightBehavior

--- Scroll Shooter/Factories/SmallEnemyFactory.cs
+++ Scroll Shooter/Factories/SmallEnemyFactory.cs
@@ -1,10 +1,11 @@
-﻿namespace ScrollShooter
+namespace ScrollShooter
 {
     class SmallEnemyFactory : EnemyFactory
     {
         public override Enemy CreateEnemy(int x, int y, int speed, IEnemyBehavior behavior)
         {
-            var enemy = new SmallEnemy(x, y, speed, new StraightBehavior());
+            var actualBehavior = behavior ?? new StraightBehavior();
+            var enemy = new SmallEnemy(x, y, speed, actualBehavior);
             enemy.HP = 1;
             enemy.Symbol = "[X]";
             return enemy;
